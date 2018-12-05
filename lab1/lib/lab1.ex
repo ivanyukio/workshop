07ac0bc65fcc2f32f8cef1d9a9809cd1b41a6371
Ivan Birkman <ivan.yk14@gmail.com>
2018-12-05 18:16:02 +0100

    Add lab1 solutions

--- lab1/lib/lab1.ex
+++ lab1/lib/lab1.ex
@@ -1,21 +1,68 @@
 defmodule Lab1 do
-  def first_and_third(_list) do
-    raise "not implemented yet"
+  def first_and_third([]), do: nil
+
+  def first_and_third([first, _second, third | _]) do
+    {first, third}
   end
 
-  def drop_three(_list) do
-    raise "not implemented yet"
+  ###
+
+  def drop_three([]), do: []
+
+  def drop_three([_first, _second, _third | tail]) do
+    tail
   end
 
-  def sum(_list) do
-    raise "not implemented yet"
+  ###
+
+  def sum(list, total \\ 0)
+
+  def sum([num | tail], total) do
+    sum(tail, num + total)
   end
 
-  def min(_list) do
-    raise "not implemented yet"
+  def sum([], total) do
+    total
   end
 
-  def average(_list) do
-    raise "not implemented yet"
+  ###
+
+  def find_min(list, match \\ nil)
+
+  def find_min([num | tail], nil) do
+    find_min(tail, num)
+  end
+
+  def find_min([num | tail], match) do
+    new_match = cond do
+      num < match ->
+        num
+      true ->
+        match
+    end
+
+    find_min(tail, new_match)
+  end
+
+  def find_min([], match) do
+    match
+  end
+
+  ###
+
+  def average([]), do: nil
+
+  def average(list) do
+    sum(list) / get_length(list)
+  end
+
+  def get_length(list, count \\ 0)
+
+  def get_length([_element | tail], count) do
+    get_length(tail, count + 1)
+  end
+
+  def get_length([], count) do
+    count
   end
 end
